b87bf40bae6f0b91b7444a0bf237f461e1e4ed3e
Элёр О. Бердиев <e.berdiev@egov.local>
2022-03-04 17:05:22 +0500

    main second commit

--- app/src/main/java/com/example/gitexample/MainActivity.kt
+++ app/src/main/java/com/example/gitexample/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.gitexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +12,8 @@ class MainActivity : AppCompatActivity() {
 
     fun mainBranch(){
 
+    }
+    fun mainBranch2(){
+        Toast.makeText(this, "main", Toast.LENGTH_SHORT).show()
     }
 }
