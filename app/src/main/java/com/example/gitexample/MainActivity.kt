113383f0f3fab89840fa4b2268c0bfb44e99e114
Элёр О. Бердиев <e.berdiev@egov.local>
2022-03-07 12:14:20 +0500

    new commit in main branch

--- app/src/main/java/com/example/gitexample/MainActivity.kt
+++ app/src/main/java/com/example/gitexample/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.gitexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +12,8 @@ class MainActivity : AppCompatActivity() {
     }
     fun master(){
 
+    }
+    fun main(){
+        Toast.makeText(this, "", Toast.LENGTH_SHORT).show()
     }
 }
