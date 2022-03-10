593e73316ca1d6d72149db3204abe3647af8206a
Interstellar <takl0248@gmail.com>
2022-03-10 11:08:48 +0500

    func

--- app/src/main/java/com/example/gitexample/MainActivity.kt
+++ app/src/main/java/com/example/gitexample/MainActivity.kt
@@ -9,5 +9,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-
+    private fun status(){
+        Toast.makeText(this, "", Toast.LENGTH_SHORT).show()
+    }
 }
